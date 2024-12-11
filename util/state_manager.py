78829dbfe603a506a44fc6e7850d417297f726d3
Maxence <maxencekrill@gmail.com>
2024-12-11 11:08:57 +0100

    feat: Added state manager file - MapType : Specifies the type of map generation. - MapSize : Specifies the size of the map to be generated. - StartingCondition : Specifies the starting loot each player will receive. - GameState : Defines the state of the game (start menu, pause, in-game, etc.). - MenuOptions : Used by the menu view to define which options should be displayed. Note: This commit is on the main branch and serves multiple purposes.

---
+++ util/state_manager.py
@@ -0,0 +1,69 @@
+from enum import Enum
+
+"""
+Define the different types of maps that can be generated:
+    - RICH : Generous resources dotted across the map
+    - GOLD_CENTER : All the gold is at the centre of the map
+    - TEST : For testing purposes
+"""
+class MapType(Enum):
+    RICH = 1
+    GOLD_CENTER = 2
+    TEST = 9
+
+"""
+Define the size of the map:
+    - SMALL : 120x120
+"""
+class MapSize(Enum):
+    SMALL = 0
+
+"""
+Define the ressources allocated to each player at the beginning of the game:
+    - LEAN :
+        - 50F, 200W, 50G
+        - Town Centre, 3 Villagers
+    - MEAN :
+        - 2000(F,W,G)
+        - Town Centre, 3 Villagers
+    - MARINES :
+        - 20000(F,W,G)
+        - 3 Town Centres, 15 Villagers, 2 (Barracks, Stable, Archery Range)
+"""
+class StartingCondition(Enum):
+    LEAN = 0
+    MEAN = 1
+    MARINES = 2
+
+"""
+Define the different states of the game:
+    - NOT_STARTED : The game has not started yet
+    - PLAY : The game is currently being played
+    - PAUSE : The game is paused
+    - GAME_OVER : The game is finished
+"""
+class GameState(Enum):
+    NOT_STARTED = 0
+    SETTINGS = 1
+    PLAY = 2
+    PAUSE = 3
+    GAME_OVER = 4
+
+"""
+Define the different options available in the menu:
+    - EXIT : Exit the game
+    - SETTINGS : Open the settings menu
+    - START_GAME : Start a new game
+    - LOAD_GAME : Load a saved game
+    - RESUME : Resume the game
+    - RESTART : Restart the game
+    - SAVE_GAME : Save the game
+"""
+class MenuOptions(Enum):
+    EXIT = 0
+    SETTINGS = 1
+    START_GAME = 2
+    RESUME = 3
+    RESTART = 4
+    LOAD_GAME = 5
+    SAVE_GAME = 6
