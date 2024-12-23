ef9a05283234e118d9bf4380b66bf5c8924f4967
Killian <killian.muntburon@gmail.com>
2024-12-23 15:51:24 +0100

    feat : Feat: Removing methods from resources - remove method to manage resources - remove method to  destroy/create

--- model/resources/resource.py
+++ model/resources/resource.py
@@ -12,14 +12,6 @@ class Resource:
     def __str__(self):
         return f"{self.name} ({self.letter}) - Amount: {self.amount}"
     
-    """Create a resource on the map""" 
-    def create(self, map, coordinate: Coordinate):
-        map.create_entity(self, coordinate)
-
-    """Destroy a resource on the map"""
-    def destroy(self, map):
-        map.destroy_entity(self)
-
     """Get the coordinate of the resource"""
     def get_coordinate(self):
         return self.__cordinate
@@ -36,25 +28,4 @@ class Resource:
     def get_amount(self):
         return self.__amount
     
-    """Method to manage the resource"""
-    def manage_ressource(self, action: str, value: int):
-
-        """Manage the resource based on the action.
-        
-        For action, two possibilites :
-        "add" to add ressource
-        "consume" to consume ressource
-        
-        """
-
-        if action == "add":
-            self.amount += value
-            print(f"Ajout de {self.name}, Quantité : {value}")
-        elif action == "consume":
-            if self.amount >= value:
-                self.amount -= value
-                print(f"Consommation de {self.name}, Quantité : {value}")
-            else:
-                print(f"Erreur de Ressource :{self.name} insuffisante. Disponible :{self.amount} ")
-        else:
-            print(f"Action {action} non reconnue")        
+      
