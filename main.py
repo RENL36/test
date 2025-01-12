1e3b4e37e7f32d1a1a61eed18557068f08deb4e6
Mehdi <ousegguamehdi105@gmail.com>
2025-01-12 17:04:00 +0100

    refactor: Remove main function and streamline menu controller initialization

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-from controller.game_controller import GameController
 from controller.menu_controller import MenuController
-from controller.view_controller import ViewController
 
-def main():
-    # Initialisez le menu controller
-    menu_controller = MenuController()
-
-    # Initialisez le contrôleur du jeu avec le menu_controller
-    game_controller = GameController(menu_controller)
-
-    # Créez un contrôleur de vue
-    view_controller = ViewController(game_controller)
-
-    # Démarrez la vue
-    view_controller.start_view()
-
-if __name__ == "__main__":
-    main()
+MenuController()
