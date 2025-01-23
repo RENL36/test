14ac045a554353a63d431ca9c231104782ab8775
Mehdi <ousegguamehdi105@gmail.com>
2025-01-23 11:41:59 +0100

    feat: Implement player generation and resource placement in map generation

--- controller/view_2_5D_controller.py
+++ controller/view_2_5D_controller.py
@@ -1,3 +1,10 @@
+import random
+from controller.interactions import Interactions
+from model.buildings.town_center import TownCenter
+from model.resources.gold import Gold
+from model.resources.wood import Wood
+from model.units.villager import Villager
+from util.coordinate import Coordinate
 from util.map import Map
 from util.state_manager import MapType
 from view.view_2_5D import View2_5D
@@ -23,6 +30,28 @@ class View2_5DController:
         """
         self.view = View2_5D(self.map)
         self.view.run()
+    def __generate_players(self, num_players: int, map_generation: Map):
+        """
+        Generates players for the game.
+
+        :param num_players: Number of players to generate.
+        :type num_players: int
+        :param map_generation: The game map where players will be placed.
+        :type map_generation: Map
+        """
+        from model.player.player import Player
+
+        self.players = []
+        for i in range(num_players):
+            player = Player(f"Player {i+1}",'blue')
+            self.players.append(player)
+    def get_players(self):
+        """
+        Returns the list of players.
+        :return: List of players.
+        :rtype: list[Player]
+        """
+        return self.players
 
     def __generate_map(self) -> Map:
         """
@@ -31,24 +60,104 @@ class View2_5DController:
         :return: The generated map.
         :rtype: Map
         """
-        map_generation: Map = Map(self.settings.map_size.value)
+        map_generation = Map(self.settings.map_size.value)
+        interactions = Interactions(map_generation)
+   
         match MapType(self.settings.map_type):
+            
+            ## ---- RICH MAP ----
             case MapType.RICH:
-                pass
+                # Ajout de 5% de bois sur la carte
+                for _ in range(int(self.settings.map_size.value ** 2 * 0.05)):
+                    wood = Wood()
+                    while True:
+                        coordinate = Coordinate(
+                            random.randint(0, self.settings.map_size.value - 1),
+                            random.randint(0, self.settings.map_size.value - 1)
+                        )
+                        if map_generation.check_placement(wood, coordinate):
+                            break
+                    map_generation.add(wood, coordinate)
+                    wood.set_coordinate(coordinate)
+
+                # Ajout de 0.5% d'or de façon aléatoire
+                for _ in range(int(self.settings.map_size.value ** 2 * 0.005)):
+                    gold = Gold()
+                    while True:
+                        coordinate = Coordinate(
+                            random.randint(0, self.settings.map_size.value - 1),
+                            random.randint(0, self.settings.map_size.value - 1)
+                        )
+                        if map_generation.check_placement(gold, coordinate):
+                            break
+                    map_generation.add(gold, coordinate)
+                    gold.set_coordinate(coordinate)
+
+            ## ---- GOLD CENTER MAP ----
             case MapType.GOLD_CENTER:
-                pass
+                center = Coordinate(self.settings.map_size.value // 2, self.settings.map_size.value // 2)
+                radius = int(self.settings.map_size.value * 0.05)
+
+                # Ajout d'un cercle d'or au centre de la carte
+                for x in range(center.get_x() - radius, center.get_x() + radius + 1):
+                    for y in range(center.get_y() - radius, center.get_y() + radius + 1):
+                        if (x - center.get_x()) ** 2 + (y - center.get_y()) ** 2 <= radius ** 2:
+                            coordinate = Coordinate(x, y)
+                            gold = Gold()
+                            map_generation.add(gold, coordinate)
+                            gold.set_coordinate(coordinate)
+
+                # Ajout de bois sur 5% de la carte de manière aléatoire
+                for _ in range(int(self.settings.map_size.value ** 2 * 0.05)):
+                    wood = Wood()
+                    while True:
+                        coordinate = Coordinate(
+                            random.randint(0, self.settings.map_size.value - 1),
+                            random.randint(0, self.settings.map_size.value - 1)
+                        )
+                        if map_generation.check_placement(wood, coordinate):
+                            break
+                    map_generation.add(wood, coordinate)
+                    wood.set_coordinate(coordinate)
+
+            ## ---- TEST MAP ----
             case MapType.TEST:
-                # Generate a test map 10x10 with a town center at (0,0) and a villager at (5,5)
-                from util.coordinate import Coordinate
-                from model.buildings.town_center import TownCenter
-                from model.units.villager import Villager
-                map_generation = Map(10)
-                town_center = TownCenter()
-                coordinate = Coordinate(0, 0)
-                town_center.set_coordinate(coordinate)
-                map_generation.add(town_center, coordinate)
-                villager = Villager()
-                coordinate = Coordinate(5,5)
-                villager.set_coordinate(coordinate)
-                map_generation.add(villager, coordinate)
+                map_generation = Map(120)
+                interactions = Interactions(map_generation)
+
+                # Création des joueurs
+                self.__generate_players(2, map_generation)
+
+                # Ajout de ressources à Player 1
+                self.get_players()[0].collect(Wood(), 1000)
+
+                # Initialisation du joueur 1
+                town_center1 = TownCenter()
+                interactions.place_object(town_center1, Coordinate(0, 0))
+                interactions.link_owner(self.get_players()[0], town_center1)
+                self.get_players()[0].set_max_population(
+                    self.get_players()[0].get_max_population() + town_center1.get_capacity_increase()
+                )
+
+                villager1 = Villager()
+                interactions.place_object(villager1, Coordinate(5, 5))
+                interactions.link_owner(self.get_players()[0], villager1)
+
+                # Ajout d'une tâche de construction pour le villageois 1
+                town_center3 = TownCenter()
+                #from model.tasks.build_task import BuildTask # type: ignore
+                #villager1.set_task(BuildTask(self.get_players()[0].get_command_manager(), villager1, Coordinate(6,6), town_center3))
+
+                # Initialisation du joueur 2
+                town_center2 = TownCenter()
+                interactions.place_object(town_center2, Coordinate(100, 100))
+                interactions.link_owner(self.get_players()[1], town_center2)
+                self.get_players()[1].set_max_population(
+                    self.get_players()[1].get_max_population() + town_center2.get_capacity_increase()
+                )
+
+                villager2 = Villager()
+                interactions.place_object(villager2, Coordinate(90, 90))
+                interactions.link_owner(self.get_players()[1], villager2)
+
         return map_generation
