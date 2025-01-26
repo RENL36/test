2eb1b9a490098cd2006a8df1a7c4585c1f8f3639
Huy <huynguyen2182004@gmail.com>
2025-01-26 18:20:22 +0100

    fix: fix errors in the constructor of unitsunits

--- model/units/archer.py
+++ model/units/archer.py
@@ -7,7 +7,7 @@ from model.resources.wood import Wood
 class Archer(Unit):
     """This class represents the Archer unit on the map, inheriting from Unit"""
     def __init__(self):
-        super().__init__("Archer", "a", 30,{ Wood: 25, Gold: 45 }, 35, 4, 4)
+        super().__init__("Archer", "a", 30,{ Wood(): 25, Gold(): 45 }, 35, 4, 4)
         super().set_range(4)
     
         
